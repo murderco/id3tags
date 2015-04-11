848c0c215b07b6dcb208a0ea209976063b3ea822
Xavier Obezo <xavier.obezo@gmail.com>
2015-04-11 12:22:18 -0700

    Added 'if' statements

--- main.py
+++ main.py
@@ -16,27 +16,39 @@ def SongPath(artist):
     return outputList
 
 
-def info():
+def info(WriteToFile = False):
     items = ["TPE1", "TALB", "TIT2", "TRCK", "USLT"]
-
+    # Artist, Album Name, Track Name, Track Number, Lyrics
     songList = SongPath("")
 
-    fname = open(mode="w",name="Track Info.txt")
+    fname = open(mode="w", name="Track Info.txt")
 
     broken = []
     for song in songList:
         audio = ID3(song.split("\n")[0])
         for item in items:
             if audio.getall(item) != [] or item == "TIT2":
-                fname.write("{}\n".format(audio.getall(item)))
+                if WriteToFile:
+                    fname.write("{}\n".format(audio.getall(item)))
+                else:
+                    print(audio.getall(item))
+
+
                 if item == "TIT2" and audio.getall(item) == []:
                     broken.append(song)
         audio.save
 
-        fname.write("\n\n\n")
+        if WriteToFile:
+            fname.write("\n\n\n")
+        else:
+            print("\n\n\n")
 
-    fname.write("{}\n".format(broken))
-    fname.write("{}\n".format(len(broken)))
+    if WriteToFile:
+        fname.write("{}\n".format(broken))
+        fname.write("{}\n".format(len(broken)))
+    else:
+        print(broken)
+        print(len(broken))
 
 
 if __name__ == "__main__":
