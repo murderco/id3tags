0edead2fd020b1a3ab2f051d0d54bece6189d1e0
isaacobezo <isaac.obezo@gmail.com>
2015-04-11 12:56:41 -0400

    moving the functionality into a 'main' module

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+""" Here is the description of the tool
+"""
 from playlist_mods import *
 from mutagen.id3 import ID3
 
@@ -7,12 +10,17 @@ items = ["TALB", "TBPM", "TCMP", "TCOM", "TCOP", "TENC", "TEXT", "TLEN", "TMED",
 songList = playlist("Completely Random.m3u")[0]
 
 
+def clean_id3s():
+    for song in songList:
+        audio = ID3(song.split("\n")[0])
+        for item in items:
+            if audio.getall(item) != [] or item == "TLEN":
+                print(audio.getall(item))
+        audio.save
 
-for song in songList:
-    audio = ID3(song.split("\n")[0])
-    for item in items:
-        if audio.getall(item) != [] or item == "TLEN":
-            print(audio.getall(item))
-    audio.save
+        print("\nNext Song\n")
 
-    print("\nNext Song\n")
+
+
+if __name__ == "__main__":
+    clean_id3s()
