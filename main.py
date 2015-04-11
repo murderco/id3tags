7188533b1c35ac0f109af6b4e9dae0e0eb9bdfa7
Xavier Obezo <xavier.obezo@gmail.com>
2015-04-11 11:01:07 -0700

    cleaning up merging

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 """ Here is the description of the tool
 """
-from playlist_mods import *
 from mutagen.id3 import *
-from time import *
 import os
 
 def SongPath(artist):
@@ -16,22 +14,9 @@ def SongPath(artist):
     return outputList
 
 
-
-#items = ["TALB", "USLT", "TBPM", "TCMP", "TCOM", "TCOP", "TENC", "TEXT", "TLEN", "TMED", "TMOO", "TIT2", "TIT3", "TPE1", "TPE2", "TPE3", "TPE4", "TPOS", "TPUB", "TRCK", "TOLY", "TSO2", "TSOA", "TSOC", "TSOP", "TSOT", "TSRC", "TSST", "TLAN"]
-
-<<<<<<< HEAD
-#items = ["TALB", "USLT", "TIT2", "TPE1", "TRCK"]
-
-items = ["TPE1", "TALB", "TIT2", "TRCK", "USLT"]
-# Playlists pointing to my music library
-#songList = playlist("Bad Day.m3u")[0]
-"""
-# For music files in folder
-songList = ["/Users/Xavier/Dropbox/Programming/Python/Trial/02 - Unbreakable.mp3",
-            "/Users/Xavier/Dropbox/Programming/Python/Trial/02 Don't Drag Me Down.mp3"]
-"""
-
 def info():
+    items = ["TPE1", "TALB", "TIT2", "TRCK", "USLT"]
+
     songList = SongPath("")
 
     fname = open(mode="w",name="Track Info.txt")
@@ -50,21 +35,3 @@ def info():
 
     fname.write("{}\n".format(broken))
     fname.write("{}\n".format(len(broken)))
-
-info()
-=======
-def clean_id3s():
-    for song in songList:
-        audio = ID3(song.split("\n")[0])
-        for item in items:
-            if audio.getall(item) != [] or item == "TLEN":
-                print(audio.getall(item))
-        audio.save
-
-        print("\nNext Song\n")
-
-
-
-if __name__ == "__main__":
-    clean_id3s()
->>>>>>> 552f724c01429fb0be74b7d6b5c7040130398ff2
