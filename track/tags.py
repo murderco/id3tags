ac7169772e8df82c188f1349ecf547f9508f9216
Xavier Obezo <xavier.obezo@gmail.com>
2015-04-11 18:40:04 -0700

    Added the variables

--- track/tags.py
+++ track/tags.py
@@ -1,7 +1,20 @@
 __author__ = 'Xavier'
 
-class Tags(object):
+from mutagen.id3 import *
 
-    def __init__(self, tags={}, tag_names=[] ):
-        self.tags = tags
-        self.tag_names = tag_names
+class Track(object):
+
+    def __init__(self,object):
+        self.artist_name = object.getall("TPE1")
+        self.album_name = object.getall("TALB")
+        self.track_name = object.getall("TIT2")
+        self.track_number = object.getall("TRCK")
+        self.lyrics = object.getall("USLT")
+
+
+
+
+Killer = Track(ID3("/Users/Xavier/Music/Library/10 Years/Minus the Machine/01 - Minus The Machine.mp3"))
+print(Killer.artist_name[0])
+print(Killer.album_name[0])
+print(Killer.track_name[0])
